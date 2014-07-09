931e7391ed21d22e4f66ac3e95cacd794643ab55
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-07-09 10:33:34 +0000

    Adding apache and application_python

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,15 @@
 # Encoding: utf-8
-name             'pythonstack'
-maintainer       'Rackspace'
+name 'pythonstack'
+maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
-license          'Apache 2.0'
-description      'Installs/Configures pythonstack'
+license 'Apache 2.0'
+description 'Installs/Configures pythonstack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version '0.1.0'
+
+depends 'apache2', '~> 1.10'
+depends 'application'
+depends 'application_python'
+depends 'python'
+depends 'chef-sugar'
+depends 'git'
---
+++ recipes/application_python.rb
@@ -0,0 +1,38 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: pythonstack
+# Recipe:: application_python
+#
+# Copyright 2014, Rackspace UK, Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'pythonstack::default'
+include_recipe 'pythonstack::apache'
+include_recipe 'pythonstack::gluster' if node.deep_fetch['rackspace_gluster']['config']['server']['glusters'].values[0].key?('nodes')
+include_recipe 'chef-sugar'
+include_recipe 'python'
+
+node['apache']['sites'].each do | site_name |
+  site_name = site_name[0]
+
+  application site_name do
+    path node['apache']['sites'][site_name]['docroot']
+    owner node['apache']['user']
+    group node['apache']['group']
+    deploy_key node['apache']['sites'][site_name]['deploy_key']
+    repository node['apache']['sites'][site_name]['repository']
+    revision node['apache']['sites'][site_name]['revision']
+  end
+end
