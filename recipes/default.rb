7eb01b8fdf14dc9c7b251c389270c4b6abd5eced
Matthew Thode <mthode@mthode.org>
2014-07-28 18:13:26 -0500

    multi site first pass
    this is a large commit :D
    sets up DBs and DB users per DB per vhost
    install vhost per site
    general cleanup of code in application_python

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,21 +17,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Include the necessary recipes.
-
-case node['platform_family']
-when 'debian'
-  %w(platformstack::monitors platformstack::iptables apt chef-sugar python).each do |recipe|
-    include_recipe recipe
-  end
-when 'rhel'
-  %w(platformstack::monitors platformstack::iptables yum chef-sugar python::package python::pip).each do |recipe|
-    include_recipe recipe
-  end
-  python_pip 'distribute' do
-    action 'install'
-    version '0.6.16'
-  end
-  include_recipe 'python::virtualenv'
-end
