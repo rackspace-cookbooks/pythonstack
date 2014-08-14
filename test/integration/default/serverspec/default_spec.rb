32cdedf700d8466a7afd89a45e32199c1a1f188e
Matthew Thode <mthode@mthode.org>
2014-08-14 15:53:03 -0500

    test the right port

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -60,7 +60,7 @@ describe service('rabbitmq-server') do
   it { should be_enabled }
   it { should be_running }
 end
-describe port(6872) do
+describe port(5672) do
   it { should be_listening }
 end
 
