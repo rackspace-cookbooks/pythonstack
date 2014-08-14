77be1f8fa928db6867d7139d47720a15bee5ccc5
Matthew Thode <mthode@mthode.org>
2014-08-14 16:25:36 -0500

    reddis testing

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -64,6 +64,15 @@ describe port(5672) do
   it { should be_listening }
 end
 
+# redis
+describe service('redis6379') do
+  it { should be_enabled }
+  it { should be_running }
+end
+describe port(6379) do
+  it { should be_listening }
+end
+
 # pythonstack.ini
 describe file('/etc/pythonstack.ini') do
   it { should be_file }
