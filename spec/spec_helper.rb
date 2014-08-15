adb10375191f675efe545c04f4b588650753aefe
Julien Berard <jujugrrr@gmail.com>
2014-08-15 16:00:13 +0000

    Added specs for Centos

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-      config.log_level = :debug
+      config.log_level = :warn
 end
 at_exit { ChefSpec::Coverage.report! }
