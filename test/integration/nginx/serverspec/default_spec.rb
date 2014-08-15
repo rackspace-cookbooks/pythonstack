ab3f485e8e6d951ce05aebbc0109a502f48d5d8a
Julien Berard <jujugrrr@gmail.com>
2014-08-15 15:04:36 +0000

    Added test for nginx/application_python_nginx

--- test/integration/nginx/serverspec/default_spec.rb
+++ test/integration/nginx/serverspec/default_spec.rb
@@ -19,6 +19,14 @@ describe port(80) do
 end
 
 # Uwsgi
+
+describe file('/var/run/uwsgi-example.com.pid') do
+  it { should be_file }
+end
+describe process("uwsgi") do
+      it { should be_running }
+end
+
 # FIXME Uwsgi cookbooks is using sv which is not supported by serverspec
 #describe service('uwsgi-example.com') do
 #  it { should be_enabled }
