ac82f34d2920a432a24f69aa23ad7504690963ad
Julien Berard <jujugrrr@gmail.com>
2014-08-15 12:11:59 +0000

    Added test for nginx recipe

---
+++ spec/nginx_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'pythonstack::nginx' do
+
+  context 'Ubuntu' do
+    let(:chef_run) {
+      ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
+          node.set['pythonstack']['demo']['enabled'] = true
+          node.set['platformstack']['cloud_backup']['enabled'] = false
+          node.set['platformstack']['cloud_monitoring']['enabled'] = false
+          node.set['authorization']['sudo']['users'] = ['vagrant']
+          node.set['authorization']['sudo']['passwordless'] = true
+      end.converge(described_recipe)
+    }
+      before do
+          stub_command("which nginx").and_return("/usr/bin/nginx")
+      end
+      it "creates a nginx site configuration" do
+        expect(chef_run).to render_file('/etc/nginx/sites-available/example.com').with_content('server')
+      end
+  end
+
+
+
+
+end
---
+++ test/integration/nginx/serverspec/default_spec.rb
@@ -0,0 +1,29 @@
+# Encoding: utf-8
+
+require_relative 'spec_helper'
+
+# nginx
+if os[:family] == 'RedHat'
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+else
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+describe port(80) do
+  it { should be_listening }
+end
+
+# Uwsgi
+# FIXME Uwsgi cookbooks is using sv which is not supported by serverspec
+#describe service('uwsgi-example.com') do
+#  it { should be_enabled }
+#  it { should be_running }
+#end
+#describe port(8080) do
+#  it { should be_listening }
+#end
