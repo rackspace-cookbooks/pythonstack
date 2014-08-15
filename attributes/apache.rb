c55dcb1f5940f376a43464a2a89218ce9cbd7e7e
Zachary Deptawa <zdeptawa@gmail.com>
2014-08-15 16:40:15 -0500

    adding attrib to fix nil error

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -17,6 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_attribute 'pythonstack::demo'
 
 if node['pythonstack']['demo']['enabled']
   site1 = 'example.com'
