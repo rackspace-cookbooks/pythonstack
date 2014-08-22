a2d3d4882fe5067bf24479156798042b0724979c
Matthew Thode <mthode@mthode.org>
2014-08-22 15:45:30 -0500

    more tests :D

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -46,12 +46,43 @@ describe port(3306) do
   it { should be_listening }
 end
 
+# postgresql-master
+if os[:family] == 'RedHat'
+  # process is named postgres
+  describe service('postgres') do
+    it { should be_running }
+  end
+  # service is named postgresql...
+  describe service('postgresql') do
+    it { should be_enabled }
+  end
+  describe service('postgres') do
+    it { should be_running }
+  end
+else
+  describe service('postgresql') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+  describe service('postgresql') do
+    it { should be_running }
+  end
+end
+describe port(5432) do
+  it { should be_listening }
+end
+
 # varnish
 describe service('varnish') do
   it { should be_enabled }
   it { should be_running }
 end
 describe port(6081) do
+  it { should be_listening }
+end
+
+# mongo
+describe port(27_017) do
   it { should be_listening }
 end
 
@@ -65,9 +96,14 @@ describe port(5672) do
 end
 
 # redis
-describe service('redis6379') do
-  it { should be_enabled }
+# cannot name the service redis6379 because the check uses ps, not the actual service name
+describe service('redis') do
   it { should be_running }
+end
+if os[:family] == 'RedHat'
+  describe service('redis6379') do
+    it { should be_enabled }
+  end
 end
 describe port(6379) do
   it { should be_listening }
