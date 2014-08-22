52ff838f15849dee2e94efdfcda61f55799b34d6
Julien Berard <jujugrrr@gmail.com>
2014-08-22 10:53:08 +0100

    Added further test for application_nginx suites

--- test/integration/application_python_nginx/serverspec/default_spec.rb
+++ test/integration/application_python_nginx/serverspec/default_spec.rb
@@ -2,40 +2,46 @@
 
 require_relative 'spec_helper'
 
-# nginx
-if os[:family] == 'RedHat'
+describe 'configures and runs Nginx' do
   describe service('nginx') do
     it { should be_enabled }
     it { should be_running }
   end
-else
-  describe service('nginx') do
-    it { should be_enabled }
-    it { should be_running }
+  describe port(80) do
+    it { should be_listening }
   end
-end
-describe port(80) do
-  it { should be_listening }
 end
 
 # Uwsgi
-describe file('/var/run/uwsgi-example.com.pid') do
-  it { should be_file }
-end
-describe process('uwsgi') do
-  it { should be_running }
+describe 'configures and runs Uwsgi' do
+  describe file('/var/run/uwsgi-example.com.pid') do
+    it { should be_file }
+  end
+  describe file('/etc/nginx/example.com.ini') do
+    it { should be_file }
+  end
+  describe port(8080) do
+    it { should be_listening }
+  end
+  describe process('uwsgi') do
+    it { should be_running }
+  end
 end
 
-# Deployment
-describe file('/var/run/uwsgi-example.com.pid') do
-  it { should be_file }
+describe 'configures our application' do
+  describe file('/etc/nginx/sites-available/example.com') do
+    it { should be_file }
+  end
+  describe file('/etc/nginx/sites-enabled/example.com') do
+    it { should be_linked_to '/etc/nginx/sites-available/example.com' }
+  end
+  describe file('/var/www/nginx-default/example.com') do
+      it { should be_directory }
+  end
+  describe file('/etc/pythonstack.ini') do
+    it { should be_file }
+  end
+  describe 'the app returns the expected content' do
+    it { expect(page_returns).to match(/MySQL Service/) }
+  end
 end
-
-#  FIXME: Uwsgi cookbooks is using sv which is not supported by serverspec
-# describe service('uwsgi-example.com') do
-#   it { should be_enabled }
-#   it { should be_running }
-# end
-# describe port(8080) do
-#   it { should be_listening }
-# end
