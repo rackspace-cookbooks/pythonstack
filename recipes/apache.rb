27dad1f655e9fe8928fde5acad2fd2084c848776
Jim Rosser <jarosser06@gmail.com>
2014-08-25 16:21:49 -0500

    fixed apache site loop

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -44,36 +44,34 @@ end
 listen_ports = []
 
 # Create the sites.
-node['apache']['sites'].each do | site_name |
-  site_name = site_name[0]
-  site = node['apache']['sites'][site_name]
-  listen_ports.push(site['port']) unless listen_ports.include? site['port']
+node['apache']['sites'].each do |site_name, site_opts|
+  listen_ports.push(site_opts['port']) unless listen_ports.include? site['port']
 
-  add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{site['port']} -j ACCEPT", 100, 'Allow access to apache')
+  add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{site_opts['port']} -j ACCEPT", 100, 'Allow access to apache')
 
   web_app site_name do
-    port site['port']
-    cookbook site['cookbook']
-    template site['template']
-    server_name site['server_name']
-    server_aliases site['server_alias']
-    docroot site['docroot']
-    allow_override site['allow_override']
-    errorlog site['errorlog']
-    customlog site['customlog']
-    loglevel site['loglevel']
-    script_name site['script_name']
+    port site_opts['port']
+    cookbook site_opts['cookbook']
+    template site_opts['template']
+    server_name site_opts['server_name']
+    server_aliases site_opts['server_alias']
+    docroot site_opts['docroot']
+    allow_override site_opts['allow_override']
+    errorlog site_opts['errorlog']
+    customlog site_opts['customlog']
+    loglevel site_opts['loglevel']
+    script_name site_opts['script_name']
   end
-  template "http-monitor-#{site['server_name']}" do
+  template "http-monitor-#{site_opts['server_name']}" do
     cookbook 'pythonstack'
     source 'monitoring-remote-http.yaml.erb'
-    path "/etc/rackspace-monitoring-agent.conf.d/#{site['server_name']}-http-monitor.yaml"
+    path "/etc/rackspace-monitoring-agent.conf.d/#{site_opts['server_name']}-http-monitor.yaml"
     owner 'root'
     group 'root'
     mode '0644'
     variables(
-      http_port: site['port'],
-      server_name: site['server_name']
+      http_port: site_opts['port'],
+      server_name: site_opts['server_name']
     )
     notifies 'restart', 'service[rackspace-monitoring-agent]', 'delayed'
     action 'create'
