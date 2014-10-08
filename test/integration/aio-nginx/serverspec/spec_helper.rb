10c8faab6c82b34a40bb71ce09cfc099806c7f98
Jacob Dearing <jacob.dearing@gmail.com>
2014-10-08 13:31:28 -0500

    rubocop fixes

--- test/integration/aio-nginx/serverspec/spec_helper.rb
+++ test/integration/aio-nginx/serverspec/spec_helper.rb
@@ -5,10 +5,10 @@ require 'net/http'
 set :backend, :exec
 set :path, '/sbin:/usr/local/sbin:/bin:/usr/bin:$PATH'
 
-def page_returns(url = 'http://localhost/')
+def page_returns(url = 'http://localhost/', host = 'example.com')
   uri = URI.parse(url)
   http = Net::HTTP.new(uri.host, uri.port)
   req = Net::HTTP::Get.new(uri.request_uri)
-  req.initialize_http_header({'Host' => 'example.com'})
+  req.initialize_http_header('Host' => host)
   http.request(req).body
 end
